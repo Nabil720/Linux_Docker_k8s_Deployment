3bda4a6a40719357cb3c52ab7e25de05c04eaab2
Nabil720 <nabilfaruk6@gmail.com>
2025-12-17 11:54:53 +0600

    fix

--- kindergarten-registry_k8s/employeeservice/main.go
+++ kindergarten-registry_k8s/employeeservice/main.go
@@ -63,7 +63,7 @@ func main() {
 	}
 
 	// Employee Routes
-	http.HandleFunc("/add-employee", func(w http.ResponseWriter, r *http.Request) {   // Here using this  "/emp/add-employee" path , we POST in  the Add-employee service 
+	http.HandleFunc("/emp/add-employee", func(w http.ResponseWriter, r *http.Request) {   // Here using this  "/emp/add-employee" path , we POST in  the Add-employee service 
 		enableCors(w)
 		if r.Method == http.MethodOptions {
 			return
@@ -71,7 +71,7 @@ func main() {
 		apmMiddleware(handlers.AddEmployee, "POST /add-employee")(w, r)
 	})
 
-	http.HandleFunc("/employees", func(w http.ResponseWriter, r *http.Request) {    // Here using this  "/emp/employees" path , we GET employees  
+	http.HandleFunc("/emp/employees", func(w http.ResponseWriter, r *http.Request) {    // Here using this  "/emp/employees" path , we GET employees  
 		enableCors(w)
 		if r.Method == http.MethodOptions {
 			return
@@ -79,7 +79,7 @@ func main() {
 		apmMiddleware(handlers.GetEmployees, "GET /employees")(w, r)
 	})
 
-	http.HandleFunc("/delete-employee", func(w http.ResponseWriter, r *http.Request) {   // Here using this  "/emp/delete-employee" path , we DELETE in  the Employees service 
+	http.HandleFunc("/emp/delete-employee", func(w http.ResponseWriter, r *http.Request) {   // Here using this  "/emp/delete-employee" path , we DELETE in  the Employees service 
 		enableCors(w)
 		enableCors(w)
 		if r.Method == http.MethodOptions {
@@ -92,7 +92,7 @@ func main() {
 		apmMiddleware(handlers.DeleteEmployee, "DELETE /delete-employee")(w, r)
 	})
 
-	http.HandleFunc("/update-employee", func(w http.ResponseWriter, r *http.Request) {    // Here using this  "/emp/update-employee" path , we PUT in  the Employees service 
+	http.HandleFunc("/emp/update-employee", func(w http.ResponseWriter, r *http.Request) {    // Here using this  "/emp/update-employee" path , we PUT in  the Employees service 
 		enableCors(w)
 		if r.Method == http.MethodOptions {
 			return
