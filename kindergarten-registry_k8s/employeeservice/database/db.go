c773704282bcd9cfe6057856deeb36b6530ce4db
Nabil720 <nabilfaruk6@gmail.com>
2026-01-14 16:41:42 +0600

    fix

--- kindergarten-registry_k8s/employeeservice/database/db.go
+++ kindergarten-registry_k8s/employeeservice/database/db.go
@@ -1,61 +1,45 @@
 package database
 
 import (
-    "context"
-    "fmt"
-    "log"
-    "os"
-    "time"
+	"context"
+	"log"
+	"os"
+	"time"
 
-    "go.mongodb.org/mongo-driver/mongo"
-    "go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var Client *mongo.Client
 var Database *mongo.Database
 
 func Connect() error {
-    // Environment variables থেকে MongoDB URI নিবে
-    // (Vault থেকে main.go-তে already set করা হয়েছে)
-    connectionString := os.Getenv("MONGODB_URI")
-    if connectionString == "" {
-        log.Fatal("MONGODB_URI environment variable is not set")
-    }
+	// Environment variables থেকে MongoDB URI নিবে
+	// (Vault থেকে main.go-তে already set করা হয়েছে)
+	connectionString := os.Getenv("MONGODB_URI")
+	if connectionString == "" {
+		log.Fatal("MONGODB_URI environment variable is not set")
+	}
 
-    databaseName := os.Getenv("DATABASE_NAME")
-    if databaseName == "" {
-        databaseName = "kindergarten"
-    }
+	databaseName := os.Getenv("DATABASE_NAME")
+	if databaseName == "" {
+		databaseName = "kindergarten"
+	}
 
-    ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-    defer cancel()
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
 
-    clientOptions := options.Client().ApplyURI(connectionString)
-    client, err := mongo.Connect(ctx, clientOptions)
-    if err != nil {
-        return fmt.Errorf("failed to connect to MongoDB: %v", err)
-    }
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(connectionString))
+	if err != nil {
+		return err
+	}
 
-    // Ping the database to verify connection
-    if err := client.Ping(ctx, nil); err != nil {
-        return fmt.Errorf("failed to ping MongoDB: %v", err)
-    }
-
-    Client = client
-    Database = client.Database(databaseName)
-    log.Println("Connected to MongoDB successfully!")
-    return nil
+	Client = client
+	Database = client.Database(databaseName)
+	log.Println("Connected to MongoDB successfully!")
+	return nil
 }
 
 func GetCollection(collectionName string) *mongo.Collection {
-    return Database.Collection(collectionName)
-}
-
-func Disconnect() {
-    if Client != nil {
-        ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-        defer cancel()
-        Client.Disconnect(ctx)
-        log.Println("MongoDB connection closed")
-    }
+	return Database.Collection(collectionName)
 }
